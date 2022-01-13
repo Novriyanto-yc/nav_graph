e90ca7b937bfc8d76c69df1951a063e0b7374077
novriyanto <novriyanto@bareksa.com>
2022-01-13 14:59:33 +0700

    Add Log

--- app/src/main/java/com/example/navgraph/fragments/BlankAFragment.kt
+++ app/src/main/java/com/example/navgraph/fragments/BlankAFragment.kt
@@ -1,6 +1,7 @@
 package com.example.navgraph.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,6 +33,8 @@ class BlankAFragment : Fragment() {
             val direction = BlankAFragmentDirections.actionBlankAFragmentToBlankCFragment()
             findNavController().navigate(direction)
         }
+
+        Log.d("ASAS", "ASASSAS")
 
     }
 
